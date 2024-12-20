0a0de41b1448dee553dc8b309de661b586636e42
Thinakaran <thinakaranmanohar@yahoo.com>
2024-12-20 22:59:04 +0530

    Hello

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/spotmix-downloader/",
+  build: {
+    outDir: 'dist', // Ensure correct output folder
+  },
 });
