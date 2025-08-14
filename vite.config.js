41dc7a96976be121724223392753debc0ba8b989
Dheena <Dheenamanohar@gmail.com>
2025-08-14 09:56:33 +0530

    Automated updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Polyfill for Node build environment
+if (!globalThis.crypto) {
+  globalThis.crypto = require('node:crypto').webcrypto;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
